0ca94a59100b7a6003a151a20365936c95d7c001
MauriDiPietro <dipietro.jm@gmail.com>
2025-11-12 22:27:23 -0300

    clase01

---
+++ 01 - PROCESO PRINCIPAL - GLOBAL & CHILD PROCESS/01-process/index.js
@@ -0,0 +1,13 @@
+console.log(process.cwd());
+
+console.log(process.pid);
+
+console.log(process.version);
+
+console.log(process.memoryUsage());
+
+console.log(process.platform);
+
+process.exit(1)
+
+console.log('hola');
